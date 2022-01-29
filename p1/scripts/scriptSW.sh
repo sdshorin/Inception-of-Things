647de0b20f80f3b163802a745d471b5e9c62ad5e
Shorin Sergey <serres123@ya.ru>
2022-01-29 19:32:24 +0300

    finish p1?

--- p1/scripts/scriptSW.sh
+++ p1/scripts/scriptSW.sh
@@ -16,3 +16,7 @@ scp -o StrictHostKeyChecking=no root@192.168.42.110:/var/lib/rancher/k3s/server/
 # cp /vagrant/token ~/token
 #curl -sfL https://get.k3s.io | sh -
 
+export INSTALL_K3S_EXEC="agent --server 192.168.42.110 --node-ip 192.168.42.111 --token-file /home/vagrant/token"
+export K3S_KUBECONFIG_MODE="644"
+curl -sfL https://get.k3s.io | sh -
+
