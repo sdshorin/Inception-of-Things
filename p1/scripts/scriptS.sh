842a96ef3a6d896e42a6941766ffae95209f8a92
Shorin Sergey <serres123@ya.ru>
2022-01-29 17:20:57 +0300

    update S

--- p1/scripts/scriptS.sh
+++ p1/scripts/scriptS.sh
@@ -8,9 +8,10 @@ cat /home/vagrant/.ssh/key.pub  >> /home/vagrant/.ssh/authorized_keys
 sudo mkdir /root/.ssh
 cat  /home/vagrant/.ssh/key.pub >> /root/.ssh/authorized_keys
 
+yum install -y net-tools
 
-export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644"
+export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip 192.168.42.110"
 export K3S_KUBECONFIG_MODE="644"
 curl -sfL https://get.k3s.io | sh -
 
-
+# k3s kubectl get nodes -o wide
