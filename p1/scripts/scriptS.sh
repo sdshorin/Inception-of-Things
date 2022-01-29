cd3b424b6a026a0b897799c12df2179ddce4b9eb
Shorin Sergey <serres123@ya.ru>
2022-01-29 19:27:28 +0300

    fix file transport

--- p1/scripts/scriptS.sh
+++ p1/scripts/scriptS.sh
@@ -4,14 +4,16 @@
 # iception
 
 echo "Hi here!!"
-cat /home/vagrant/.ssh/key.pub  >> /home/vagrant/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub  >> /home/vagrant/.ssh/authorized_keys
 sudo mkdir /root/.ssh
-cat  /home/vagrant/.ssh/key.pub >> /root/.ssh/authorized_keys
+cp -R  /home/vagrant/.ssh/*  /root/.ssh
 
 yum install -y net-tools
 
 export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip 192.168.42.110"
 export K3S_KUBECONFIG_MODE="644"
 curl -sfL https://get.k3s.io | sh -
+
+# cp /var/lib/rancher/k3s/server/node-token /vagrant/token
 
 # k3s kubectl get nodes -o wide
--- p1/scripts/scriptSW.sh
+++ p1/scripts/scriptSW.sh
@@ -4,13 +4,15 @@
 # iception
 
 echo "Hi here!!"
-cat /home/vagrant/.ssh/key.pub  >> /home/vagrant/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub  >> /home/vagrant/.ssh/authorized_keys
 sudo mkdir /root/.ssh
-cat  /home/vagrant/.ssh/key.pub >> /root/.ssh/authorized_keys
+cp -R  /home/vagrant/.ssh/*  /root/.ssh
+
 
 yum install -y net-tools
 
-scp -o StrictHostKeyChecking=no root@192.168.42.110:/var/lib/rancher/k3s/server/node-token token
-
+scp -o StrictHostKeyChecking=no root@192.168.42.110:/var/lib/rancher/k3s/server/node-token /home/vagrant/token
+# cat /vagrant/token
+# cp /vagrant/token ~/token
 #curl -sfL https://get.k3s.io | sh -
 
