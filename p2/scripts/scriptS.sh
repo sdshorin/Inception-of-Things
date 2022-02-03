a8ef976ab5f3d1846c47ad7594fae24e596cebf5
GoncharovAndrey <goncharov.andrey.94@gmail.com>
2022-02-03 04:01:40 +0300

    p2. add ingress

--- p2/scripts/scriptS.sh
+++ p2/scripts/scriptS.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# ssh-keygen -t rsa
-# iception
-
 echo "Hi here!!"
 cat /home/vagrant/.ssh/id_rsa.pub  >> /home/vagrant/.ssh/authorized_keys
 sudo mkdir /root/.ssh
 cp -R  /home/vagrant/.ssh/*  /root/.ssh
 
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
 
 yum install -y net-tools
@@ -18,8 +14,23 @@ export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip 192.168.42
 export K3S_KUBECONFIG_MODE="644"
 curl -sfL https://get.k3s.io | sh -
 
-#kubectl apply -f  /home/vagrant/test.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/app1/configmap.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/app1/deployment.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/app2/configmap.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/app2/deployment.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/app3/configmap.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/app3/deployment.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/services.yaml
+/usr/local/bin/kubectl apply -f /home/vagrant/confs/ingress.yaml
 
-# cp /var/lib/rancher/k3s/server/node-token /vagrant/token
+#/usr/local/bin/kubectl create namespace p2
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/app1/configmap.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/app1/deployment.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/app2/configmap.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/app2/deployment.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/app3/configmap.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/app3/deployment.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/services.yaml
+#/usr/local/bin/kubectl -n p2 apply -f /home/vagrant/confs/ingress.yaml
 
 # k3s kubectl get nodes -o wide
