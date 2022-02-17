f33d02d4b5c1681e9c8a91e9e922633245971ec5
Shorin Sergey <serres123@ya.ru>
2022-02-17 15:38:44 +0300

    try to connect

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -18,11 +18,22 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 echo "Устанавливаем k3d"
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 k3d cluster create mycluster
+# k3d cluster create mycluster -p 8080:80@loadbalancer
 
 echo "Устанавливаем AgroCD"
 /usr/local/bin/kubectl create namespace argocd
 /usr/local/bin/kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-/usr/local/bin/kubectl port-forward svc/argocd-server -n argocd 8080:443
+/usr/local/bin/kubectl wait --for=condition=available deployment --all -n argocd --timeout=60m
+/usr/local/bin/kubectl wait --for=condition=Ready pods --all -n argocd --timeout=60m
+
+echo "port-forward 8080:80"
+/usr/local/bin/kubectl port-forward svc/argocd-server -n argocd 8080:80 & #  --address=0.0.0.0 &
+echo "port-forward 8443:443"
+/usr/local/bin/kubectl port-forward svc/argocd-server -n argocd 8443:443 & # --address=0.0.0.0 &
 
 echo "Password ArgoCD"
 /usr/local/bin/kubectl get pods -n argocd | grep argocd-server
+
+sudo mkdir /root/.kube
+sudo cp /home/vagrant/.kube/config  /root/.kube/config
+# /usr/local/bin/kubectl port-forward svc/argocd-server -n argocd 8080:443&
